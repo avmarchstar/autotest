019016eca5f741bcc11feeab3757e723cfadcf83
avmarchstar <avmarchstar@gmail.com>
2022-04-05 20:08:27 +0300

    auto6

---
+++ src/test/java/com/auto6/helpers/DP.java
@@ -0,0 +1,16 @@
+package com.auto6.helpers;
+
+import org.testng.annotations.DataProvider;
+
+public class DP {
+
+    @DataProvider(name = "p_credentials")
+    private static Object [][] m_credentials(){
+        return new Object[][] {
+                {"","","Epic sadface: Username is required"},
+                {"locked_out_user","secret_sauce","Epic sadface: Sorry, this user has been locked out."},
+                {"standard_user","","Epic sadface: Password is required"},
+                {"incorrect","incorrect","Epic sadface: Username and password do not match any user in this service"}
+        };
+    }
+}
---
+++ src/test/java/com/auto6/pages/LoginPage.java
@@ -0,0 +1,85 @@
+package com.auto6.pages;
+
+import com.auto6.helpers.BasePage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+   public final static String LOGIN = "login_credentials";
+   public final static String PASSWORD = "login_password";
+
+
+
+    @FindBy(css = "input#login-button")
+    WebElement loginButton;
+    @FindBy (id = "user-name")
+    WebElement usernameField;
+    @FindBy(name = "password")
+    WebElement passwordFild;
+    @FindBy(xpath = "//h3[@data-test = 'error']")
+    WebElement error;
+    @FindBy(className = "error-button")
+    WebElement errorButton;
+
+    Logger log = Logger.getLogger(this.getClass());
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        log.info("Set up login page.");
+    }
+
+    public String getFirstValue(String className) {
+        log.info("Get value from "+className);
+        String values = byClassName(className).getText();
+        String value = values.split("\n")[1];
+        log.info("first value = "+value);
+
+        return value;
+    }
+
+    public boolean isErrorPresent() {
+        try {
+            return error.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+
+    }
+
+    public WebElement getErrorButton() {
+        return errorButton;
+    }
+    public String getErrorText() {
+        return error.getText();
+    }
+
+    public WebElement getLoginButton() {
+        return loginButton;
+    }
+
+    public InventoryPage login(String username, String password){
+        log.info("Login with username: "+username+" password: "+password);
+        clearInputField(usernameField);
+        usernameField.sendKeys(username);
+        clearInputField(passwordFild);
+        passwordFild.sendKeys(password);
+        loginButton.click();
+
+        return new InventoryPage(driver);
+    }
+
+    private void clearInputField(WebElement element) {
+        int length = element.getAttribute("value").length();
+
+        do {
+            element.sendKeys(Keys.BACK_SPACE);
+        }while(element.getAttribute("value").length()>0 && element.getAttribute("value").length()<length);
+
+    }
+
+}
